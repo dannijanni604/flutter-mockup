8b32e19550addebe55f3845d646fbc3e2f577c17
adnan-wb <aaservices604@gmail.com>
2024-10-27 14:38:33 +0500

    updated-user-model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,1 +1,12 @@
+class User {
+  final int userId;
+  final String username;
+  final String userMail;
+  final String userImage;
 
+  User(
+      {required this.userId,
+      required this.username,
+      required this.userMail,
+      required this.userImage});
+}
