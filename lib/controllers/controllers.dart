f24a332a82de70cf0da06a5ba38b855f225c8b6e
adnan-wb <aaservices604@gmail.com>
2024-10-30 10:58:00 +0500

    profile-added

--- lib/controllers/controllers.dart
+++ lib/controllers/controllers.dart
@@ -3,3 +3,4 @@ export 'bindings/home_binding.dart';
 export 'controllers/login_controller.dart';
 export 'controllers/splash_controller.dart';
 export 'controllers/feed_controller.dart';
+export 'controllers/profile_controller.dart';
---
+++ lib/views/profile/profile_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../../controllers/controllers.dart';
+
+class ProfileView extends GetView<ProfileController> {
+  const ProfileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
